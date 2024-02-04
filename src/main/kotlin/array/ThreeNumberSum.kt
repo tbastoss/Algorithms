1bb890bbcc7a891a31a7cdd04e3e70ce723f0b32
Thiago Bastos <thiagobastos@Thiagos-MacBook-Pro.local>
2024-02-04 16:57:19 -0500

    ThreeSumNum - Time/Space analyzes and new example

--- src/main/kotlin/array/ThreeNumberSum.kt
+++ src/main/kotlin/array/ThreeNumberSum.kt
@@ -3,9 +3,14 @@ package array
 fun main() {
     val array = mutableListOf(10, 2, -1, 3, 99, 13) // [-1, 2, 3, 10, 13, 99]
     val targetSum = 4
-    print(threeNumberSum(array, targetSum))
+    val array1 = mutableListOf(12, 3, 1, 2, -6, 5, -8, 6)
+    val targetSum1 = 0
+    print(threeNumberSum(array1, targetSum1))
 }
 
+/**
+ * This is a O(n2) for time and O(n) space solution.
+ */
 fun threeNumberSum(array: MutableList<Int>, targetSum: Int): List<List<Int>> {
     array.sort()
     val triplets = mutableListOf<List<Int>>()
