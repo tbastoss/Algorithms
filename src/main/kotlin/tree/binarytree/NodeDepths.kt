a6871363f4db06ccaa015ce12940b87b257c238d
Thiago Bastos <thiagobastos@Thiagos-MacBook-Pro.local>
2024-02-04 23:27:57 -0500

    NodeDepths - Time/Space analyzes

--- src/main/kotlin/tree/binarytree/NodeDepths.kt
+++ src/main/kotlin/tree/binarytree/NodeDepths.kt
@@ -3,6 +3,15 @@ package tree.binarytree
 import tree.model.BinaryTree
 import tree.model.buildTree
 
+fun main() {
+    val tree = buildTree()
+    println(nodeDepths(tree))
+}
+
+/**
+ * On average, it is O(n) Time and O(h) Space, where n is the number of nodes in the
+ * binary tree and h is the height of the binary tree
+ */
 fun nodeDepths(root: BinaryTree): Int {
     return sumOfDepths(root, 0)
 }
@@ -10,9 +19,4 @@ fun nodeDepths(root: BinaryTree): Int {
 private fun sumOfDepths(node: BinaryTree?, depth: Int): Int {
     if (node == null) return 0
     return depth + sumOfDepths(node.left, depth+1) + sumOfDepths(node.right, depth+1)
-}
-
-fun main() {
-    val tree = buildTree()
-    println(nodeDepths(tree))
 }
